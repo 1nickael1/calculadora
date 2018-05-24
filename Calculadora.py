100c62de5f0528aedf76957e61b99cb4e547ded2
1nickael1 <qaznickael@hotmail.com>
2018-05-24 01:40:00 -0300

    remoção de redundancias

--- Calculadora.py
+++ Calculadora.py
@@ -7,9 +7,9 @@ opcoes()
 i = 0
 while i == 0:
     x = int(input('Deseja fazer outra equação?\n1 - Sim\n2 - Não\n\n'))
-    if (x < 1 or x > 2):
+    if x < 1 or x > 2:
         i = 0
-    elif (x == 1):
+    elif x == 1:
         opcoes()
     else:
         print('\nFechando Programa!')
--- calculo.py
+++ calculo.py
@@ -5,64 +5,72 @@ def opcoes():
                       '\n3 - Divisão        7 - Resto da Divisão'
                       '\n4 - Multiplicação  8 - Raiz Quadrada'
                       '\n                   9 - Sair\n\n'))
-    if (opcao < 1 or opcao > 9):
+    if opcao < 1 or opcao > 9:
         print('\nEssa opção não existe!')
         opcoes()
-    elif (opcao == 1):
+    elif opcao == 1:
         soma()
-    elif (opcao == 2):
-         sub()
-    elif (opcao == 3):
-         div()
-    elif (opcao == 4):
+    elif opcao == 2:
+        subt()
+    elif opcao == 3:
+        divi()
+    elif opcao == 4:
         multi()
-    elif (opcao == 5):
+    elif opcao == 5:
         pote()
-    elif (opcao == 6):
+    elif opcao == 6:
         inte()
-    elif (opcao == 7):
+    elif opcao == 7:
         resto()
-    elif (opcao == 8):
+    elif opcao == 8:
         raiz()
-    elif (opcao == 9):
+    elif opcao == 9:
         print('\nSaindo do programa!')
         exit()
+
 
 def soma():
     n1 = int(input('\nDigite o primeiro número: '))
     n2 = int(input('\nDigite o  segundo número: '))
     print('\nA adição de {} em {} é {}\n'.format(n1, n2, (n1 + n2)))
 
-def sub():
+
+def subt():
     n1 = int(input('\nDigite o primeiro número: '))
     n2 = int(input('\nDigite o  segundo número: '))
     print('\nA Subtração de {} e {} é {}\n'.format(n1, n2, (n1 - n2)))
 
-def div():
+
+def divi():
     n1 = int(input('\nDigie o primeiro número: '))
     n2 = int(input('\nDigite o segundo número: '))
     print('\nA Divisão de {} e {} é {}\n'.format(n1, n2, (n1 / n2)))
+
 
 def multi():
     n1 = int(input('\nDigite o primeiro número: '))
     n2 = int(input('\nDigite o  segundo número: '))
     print('\nA Multiplicação de {} e {} é {}\n'.format(n1, n2, (n1 * n2)))
 
+
 def pote():
     n1 = int(input('\nDigite o primeiro número: '))
     n2 = int(input('\nDigite o  segundo número: '))
     print('\nO {} elevado a potência {} é {}\n'.format(n1, n2, (n1 ** n2)))
+
 
 def inte():
     n1 = int(input('\nDigite o primeiro número: '))
     n2 = int(input('\nDigite o  segundo número: '))
     print('\n A divisão inteira de {} e {} é {}\n'.format(n1, n2, (n1 // n2)))
 
+
 def resto():
     n1 = int(input('\nDigite o primeiro número: '))
     n2 = int(input('\nDigite o  segundo número: '))
     print('\nResto da divisão de {} e {} é {}\n'.format(n1, n2, (n1 % n2)))
 
+
 def raiz():
     n1 = int(input('\nDigite o número: '))
-    print('\nA raiz quadrada de {} é {:.3f}\n'.format(n1, (n1**(1/2))))
+    print('\nA raiz quadrada de {} é {:.3f}\n'.format(n1, (n1 ** (1 / 2))))
