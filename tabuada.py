f61f54ad1f8a3e980e00e57ff11565ac84b1d4c1
1nickael1 <qaznickael@hotmail.com>
2018-05-30 20:03:49 -0300

    att tabuada

--- tabuada.py
+++ tabuada.py
@@ -10,16 +10,5 @@ def input_int(msg):
 def conta():
       x = input_int('Digite um numero para ver sua tabuada: ')
 
-      print('------------\n'
-            '{} x  1 = {}\n'
-            '{} x  2 = {}\n'
-            '{} x  3 = {}\n'
-            '{} x  4 = {}\n'
-            '{} x  5 = {}\n'
-            '{} x  6 = {}\n'
-            '{} x  7 = {}\n'
-            '{} x  8 = {}\n'
-            '{} x  9 = {}\n'
-            '{} x 10 = {}\n'
-            '------------'.format(x, x, x, (x*2), x, (x*3), x, (x*4), x, (x*5), x, (x*6), x, (x*7), x, (x*8), x, (x*9), x, (x*10)))
-
+      for c in range(1, 11):
+          print('{} x {} = {}'.format(x, c, (x * c)))
